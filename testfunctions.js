141e44dbf26469f3f1782672666548cad967ce5f
msychova <msychova@exadel.com>
2018-05-13 21:54:39 +0300

    Commit L5

--- testfunctions.js
+++ testfunctions.js
@@ -1,4 +1,4 @@
-todoitem.addTodoItem('text0',false);
+/*todoitem.addTodoItem('text0',false);
 todoitem.addTodoItem('text1',true);
 todoitem.addTodoItem('text2',false);
 todoitem.addTodoItem('text3',true);
@@ -6,4 +6,4 @@ todoitem.editTodoItem(2,'Updated text');
 todoitem.viewTodoList('not_completed');
 todoitem.viewTodoList('completed');
 todoitem.viewTodoList('all');
-todoitem.deleteTodoItem(3);
+todoitem.deleteTodoItem(3);*/
